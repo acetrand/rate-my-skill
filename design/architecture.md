2aa24943e5cd4e5f3cd911d34e03c8dc15bf141d
Fredrik Åstrand <acetrand+dev@gmail.com>
2017-09-21 00:38:28 +0200

    Revert "Auto-generated commit"
    This reverts commit 90e372c314c582690332094546aa281864d24c14.

---
+++ design/architecture.md
@@ -0,0 +1,17 @@
+# architecture
+
+* main container component
+  * add skill component
+  * list of skills component
+  * individual skill component 
+
+* skill collection service
+  * get all skills
+  * remove skill
+  * add skill
+
+* skill class
+  * name
+  * id
+  * rate
+  * isValidRate() ? (i.e. 0<rate>=5)
---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+export class Skill {
+  id: number;
+  name: string;
+  rate: number;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'rate-my-skill';
+
+  myskill: Skill = {
+    id: 1,
+    name: 'JavaScript',
+    rate: 5
+  };
+
+  test = () => {
+    console.log('not implemented yet');
+  }
+}
