e7846b43c7c275c9d80ade73082825615e7262b4
Fredrik Åstrand <acetrand+dev@gmail.com>
2017-09-20 23:40:08 +0200

    feat(app): allow updating a single skill with inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export class Skill {
+  id: number;
+  name: string;
+  rate: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rate-my-skill';
+
+  myskill: Skill = {
+    id: 1,
+    name: 'JavaScript',
+    rate: 5
+  };
 }
