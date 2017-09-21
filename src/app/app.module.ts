1fee220ce0332cd366abd82fafd6976ba2ce29bc
Fredrik Astrand <acetrand+dev@gmail.com>
2017-09-21 10:30:39 +0200

    ui(app): use mdInput

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MdButtonModule, MdCardModule, MdListModule } from '@angular/material';
+import { MdButtonModule, MdCardModule, MdListModule, MdInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -11,10 +12,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     MdButtonModule,
     MdCardModule,
-    MdListModule
+    MdListModule,
+    MdInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
