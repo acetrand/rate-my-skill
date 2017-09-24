5726c02ed8eccc3f8b589df50b143e4d5e523396
Fredrik Åstrand <acetrand+dev@gmail.com>
2017-09-24 15:34:47 +0200

    test(e2e): fix the check header test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('rate-my-skill App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getHeaderText()).toEqual('Rate my skills');
   });
 });
